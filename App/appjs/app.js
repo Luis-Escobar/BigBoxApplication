c4452b17f3274d538df7edb99560e559c7569a3c
FrankieDC <francisco.dechoudens@upr.edu>
2013-11-25 15:05:35 -0400

    .

--- App/appjs/app.js
+++ App/appjs/app.js
@@ -2,9 +2,9 @@ var isSearchbyCat;
 
 $(document).on('pagebeforeshow', "#results", function(event, ui) {
 	if(isSearchbyCat){
-		/**
-		$.ajax({
-		url : "http://bigbox.herokuapp.com/BigBoxServer/itemsearchbycat/"+currentcid+"/"+currentcid2,
+		
+		$.ajax({										
+		url : "http://bigbox.herokuapp.com/BigBoxServer/itemsearchbycat/1/"+currentcid2,
 		contentType : "application/json",
 		success : function(data, textStatus, jqXHR) {
 			var itemList = data.items;
@@ -15,7 +15,7 @@ $(document).on('pagebeforeshow', "#results", function(event, ui) {
 		error : function(data, textStatus, jqXHR) {
 			console.log("textStatus: " + textStatus);
 			alert("Data not found!");
-		}**/
+		}
 		
 	}
 	else{
