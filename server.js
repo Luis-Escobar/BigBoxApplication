9de85f863e56f0bb85641e7ed45c6680fbf51520
luis-escobar <luis.escobar@upr.edu>
2013-12-11 03:04:51 -0400

    Bid

--- server.js
+++ server.js
@@ -760,10 +760,10 @@ client.connect(function(err) {
 		console.log("Item New: " + JSON.stringify(req.body.i_name));
 
 		var queryString =  "UPDATE items " +
-   							"SET i_name= " + req.body.i_name + ", i_model= " + req.body.i_model + ", i_year= " + req.body.i_year + ", i_info= " + req.body.i_info + 
+   							"SET i_name= " + req.body.i_name + ", i_year= " + req.body.i_year + ", i_info= " + req.body.i_info + 
        							", i_price= " + req.body.i_price + ", i_img= " + req.body.i_img + 
        							", i_shipto= " + req.body.i_shipto + ", i_shipfrom = " + req.body.i_shipfrom +  ", i_shippingprice= " + req.body.i_shippingprice  +  
-      							", i_qtyavailable= " + req.body.i_qtavailable  + ", i_bid= " + req.body.i_bid + ", cid= " + req.body.cid + ", subid= " + req.body.subid + 
+      							", i_qtyavailable= " + req.body.i_qtyavailable  + ", i_bid= " + req.body.i_bid + ", cid= " + req.body.cid + ", subid= " + req.body.subid + 
        							", ssubid= " + req.body.ssubid +
  						   " WHERE i_id = " + req.body.i_id;
 	
