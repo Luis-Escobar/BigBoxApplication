6b457480b514a49a2a87e4e8fc1e457adcfca7be
FrankieDC <francisco.dechoudens@upr.edu>
2013-12-12 06:31:39 -0400

    .

--- server.js
+++ server.js
@@ -1034,11 +1034,11 @@ client.connect(function(err) {
 	});
 	 
 	app.del('/BigBoxServer/rmvCategoryByAdmin/:rmvid', function(req, res) {
-		//var id = req.params.rmvid;
-		//console.log("DELETE item: " + id);
+		var id = req.params.rmvid;
+		console.log("DELETE item: " + id);
 		var queryString = "DELETE FROM category WHERE cid=$1";
 		
-			client.query(queryString,[rmvid], function(err, result) {
+			client.query(queryString,[req.params.rmvid], function(err, result) {
 			if (err) {
 				return console.error('error running query', err);
 			} else {		
