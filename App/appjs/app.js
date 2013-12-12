738a714902eb51e1c36150d8c5c394130b97bcfd
luis-escobar <luis.escobar@upr.edu>
2013-12-12 06:10:46 -0400

    LEt's see

--- App/appjs/app.js
+++ App/appjs/app.js
@@ -623,7 +623,7 @@ $(document).on('pagebeforeshow', "#checkout-page", function(event, ui) {
  			shippingTotal += parseFloat(item.i_shippingprice);
  			subTotal += parseFloat(item.i_price);
  			items_ship.append("<li>" + "<img src='" + item.i_img + "'/>" + "<p id='infoCart'>" + item.i_name + "</p>" + "<p> $" + item.i_price + 
- 			"</p>" + "<div class='ui-li-aside'> <pre>Qty: </pre>" + item.i_name + "</div></li>");
+ 			"</p>" + "<div class='ui-li-aside'> <pre>Qty: </pre>" + item.qtytopurchase + "</div></li>");
  //			"</p>" + "<div class='ui-li-aside'><fieldset data-role='controlgroup'>" + "<legend><pre>Qty: </pre> </legend>" + "<select name='qty' id='qty' >" + options + "</select></fieldset></div></li>");
  
  		}
